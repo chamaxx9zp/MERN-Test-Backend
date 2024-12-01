6512d0c919acc7646c7259abf42157fb11f2080c
chamaxx9zp <chamakapiyumal@gmail.com>
2024-12-01 16:15:46 +0530

    add rest api 😍

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const controller = require('./controller');
 const express = require('express');
 const cors = require('cors');
 
@@ -11,5 +12,20 @@ app.use(
     })
 )
 app.use(express.json());
+
+// Basic Route (get API)
+app.get('/users', (req, res) => {
+    controller.getUsers(users => {
+        res.send(users);
+    });
+});
+
+app.get('/user', (req, res) => {
+    const id = req.query.id;
+    controller.getUserById(id, user => {
+        res.send(user);
+    });
+});
+
 
 module.exports = app;
